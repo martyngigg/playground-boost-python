5d68ea1ceba6c75c979e0ea0b0277da54f2b1084
Martyn Gigg <martyn.gigg@gmail.com>
2024-02-09 12:22:40 +0000

    Expand example to show differences with raw pointers
    extract<const T&> requires an exact match with T so that the
    existing installed object can be referenced. In the case of
    extract<const shared_ptr<A>&>, where the Python
    object holds a shared_ptr<B>, extract would need to create a
    new shared_ptr<A> copy from the original that would not be
    a reference to existing held instance so it fails.
    Extracting by value allows a copy of the shared_ptr to be created,
    using the alias constructor, and hence extaction suceeds.

--- test/polymorphism.cpp
+++ test/polymorphism.cpp
@@ -1,4 +1,5 @@
 #include <boost/python.hpp>
+#include <iostream>
 #include <memory>
 #include <string>
 
@@ -6,13 +7,66 @@ using namespace boost;
 using namespace boost::python;
 
 struct A {
-  static std::shared_ptr<A> create() { return std::shared_ptr<A>(new A); }
-  std::string hello() { return "Just nod if you can hear me!"; }
+  virtual ~A() { std::cerr << "~A(): " << this << "\n"; }
+  virtual std::string hello() { return "Hello I'm A!"; }
 };
 
+struct B : A {
+  virtual ~B() { std::cerr << "~B(): " << this << "\n"; }
+  std::string hello() { return "Hello I'm B!"; }
+};
+
+A *create_as_A(int choice) {
+  switch (choice) {
+  case 0:
+    return new A;
+  case 1:
+    return new B;
+  default:
+    return nullptr;
+  }
+}
+
+B *create_as_B() { return new B; }
+
+void sink_A(const object &obj) {
+  A *extracted_ref = extract<A *>(obj)();
+  assert(extracted_ref);
+  std::cerr << extracted_ref->hello() << "\n";
+}
+
+std::shared_ptr<A> create_as_shared_A(int choice) {
+  switch (choice) {
+  case 0:
+    return std::shared_ptr<A>(new A);
+  case 1:
+    return std::shared_ptr<B>(new B);
+  default:
+    return std::shared_ptr<A>();
+  }
+}
+
+std::shared_ptr<B> create_as_shared_B() { return std::shared_ptr<B>(new B); }
+
+void sink_shared_A(const object &obj) {
+  auto extracted_value = extract<std::shared_ptr<A>>(obj)();
+  std::cerr << "Extracted val: " << extracted_value
+            << ", use_count: " << extracted_value.use_count() << "\n";
+  auto extracted_ref = extract<const std::shared_ptr<A> &>(obj)();
+  std::cerr << "Extracted ref: " << extracted_ref
+            << ", use_count: " << extracted_ref.use_count() << "\n";
+}
+
 BOOST_PYTHON_MODULE(polymorphism_ext) {
-  class_<A, std::shared_ptr<A>>("A", init<>())
-      .def("create", &A::create)
-      .staticmethod("create")
-      .def("hello", &A::hello);
+  class_<A>("A", init<>()).def("hello", &A::hello);
+  register_ptr_to_python<std::shared_ptr<A>>();
+  class_<B, bases<A>>("B", init<>());
+  register_ptr_to_python<std::shared_ptr<B>>();
+
+  def("create_as_A", create_as_A, return_value_policy<manage_new_object>());
+  def("create_as_B", create_as_B, return_value_policy<manage_new_object>());
+  def("sink_A", sink_A);
+  def("create_as_shared_A", create_as_shared_A);
+  def("create_as_shared_B", create_as_shared_B);
+  def("sink_shared_A", sink_shared_A);
 }
--- test/polymorphism.py
+++ test/polymorphism.py
@@ -1,6 +1,17 @@
 # Test script for stepping through creation of a Python object in Boost Python
-from polymorphism_ext import A
+from polymorphism_ext import *
 
-input("Press enter")
-a = A.create()
-print(type(a))
+#input("Press enter")
+# a = create_as_shared_A(1)
+# print(type(a))
+b = create_as_shared_B()
+# print(type(b))
+# print(b.hello())
+sink_shared_A(b)
+
+# b_as_a = create_as_A(1)
+# print(type(b_as_a))
+# sink_A(b_as_a)
+b_as_b = create_as_B()
+print(type(b_as_b))
+sink_A(b_as_b)
